f10fca456cd5335d5afa4cf74664feb02f0fabba
coding4lif3 <fed.monaco@gmail.com>
2014-10-10 01:49:58 +0200

    Added MapLocation Intent

--- app/src/main/java/com/coding4lif3/sunshine/MainActivity.java
+++ app/src/main/java/com/coding4lif3/sunshine/MainActivity.java
@@ -1,9 +1,14 @@
 package com.coding4lif3.sunshine;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.coding4lif3.sunshine.fragment.ForecastFragment;
+import com.coding4lif3.sunshine.util.Utility;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,24 +24,36 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.detail, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.action_settings:
-//                Intent intent = new Intent(this, SettingsActivity.class);
-//                startActivity(intent);
-//                return true;
-//            case R.id.action_refresh:
-//                Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_map:
+                openPreferredLocationMap();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openPreferredLocationMap() {
+        String preferredLocation = Utility.getPreferenceLocation(this);
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", preferredLocation).build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        startActivity(intent);
+    }
+
+
 }
